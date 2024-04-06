1f5b4d2b651f5161223abaaf68e06a11d5abac0d
Stefan Roeck <stefan.roeck@gmail.com>
2024-04-06 20:37:22 +0200

    parse summary data from fit file

--- src/main/kotlin/de/sroeck/tracksbackend/fit/fitToGpx.kt
+++ src/main/kotlin/de/sroeck/tracksbackend/fit/fitToGpx.kt
@@ -3,28 +3,51 @@ package de.sroeck.tracksbackend.fit
 import com.garmin.fit.*
 import java.io.ByteArrayInputStream
 
-data class FitDataPoint(val lat: Int, val lon: Int, val timestamp: DateTime, val altitude: Float?, val heartRate: Short?)
-data class FitData (val fitDataPoints: List<FitDataPoint>)
+data class FitDataPoint(
+    val lat: Int,
+    val lon: Int,
+    val timestamp: DateTime,
+    val altitude: Float?,
+    val heartRate: Short?
+)
+
+data class FitSession(
+    val startTime: DateTime,
+    val sport: Sport,
+    val totalElapsedTime: Float,
+    val totalTimerTime: Float,
+    val totalDistance: Float,
+    val totalAscent: Int,
+    val totalDescent: Int,
+    val totalCalories: Int
+)
+
+data class FitData(val fitDataPoints: List<FitDataPoint>, val fitSession: FitSession)
+
 fun parseFitFile(fitFile: ByteArray): FitData {
     val decoder = Decode()
-    val collectedDataPoints = ArrayList<FitDataPoint>();
+    val collectedDataPoints = ArrayList<FitDataPoint>()
 
     if (!decoder.checkFileIntegrity(ByteArrayInputStream(fitFile))) {
         error("provided file is not a valid fit file")
     }
     val msgBroadcaster = MesgBroadcaster(decoder)
     msgBroadcaster.addListener(RecordMesgListener { message ->
-        if (message != null && message.positionLat != null && message.positionLong != null && message.timestamp != null) {
+        if (message.positionLat != null && message.positionLong != null && message.timestamp != null) {
             collectedDataPoints.add(FitDataPoint(message.positionLat, message.positionLong, message.timestamp, message.altitude, message.heartRate))
         }
-        //println(message?.fields?.map { t -> t.name })
-        //println("${message?.timestamp}: ${message?.positionLat}:${message?.positionLong}, alt: ${message?.altitude}, heart: ${message?.heartRate}")
     })
-    msgBroadcaster.addListener(ActivityMesgListener { message ->
-        println(message?.fields?.map { t -> t.name })
-        println("${message?.type} - ${message?.totalTimerTime} - ${message?.numSessions} : ${message?.eventType}")
+
+    var fitSession: FitSession? = null
+    msgBroadcaster.addListener(SessionMesgListener { message ->
+        fitSession = FitSession(message.startTime, message.sport, message.totalElapsedTime, message.totalTimerTime, message.totalDistance, message.totalAscent, message.totalDescent, message.totalCalories)
     })
 
     decoder.read(ByteArrayInputStream(fitFile), msgBroadcaster)
-    return FitData(collectedDataPoints)
+
+    if (fitSession == null) {
+        error("No fitSession found in fit file")
+    }
+
+    return FitData(collectedDataPoints, fitSession!!)
 }
--- src/test/kotlin/de/sroeck/tracksbackend/fit/FitToGpxKtTest.kt
+++ src/test/kotlin/de/sroeck/tracksbackend/fit/FitToGpxKtTest.kt
@@ -1,5 +1,6 @@
 package de.sroeck.tracksbackend.fit
 
+import com.garmin.fit.Sport
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -13,13 +14,23 @@ class FitToGpxKtTest {
 
         bytes?.let {
             val res = parseFitFile(it)
-            assertThat(res.fitDataPoints).hasSize(22045)
+            val ( fitDataPoints, fitSession ) = res;
+            assertThat(fitDataPoints).hasSize(22045)
 
-            assertThat(res.fitDataPoints[0].lat).isEqualTo(579288959)
-            assertThat(res.fitDataPoints[0].lon).isEqualTo(104129988)
-            assertThat(res.fitDataPoints[0].timestamp.toString()).isEqualTo("Sun Dec 17 09:28:40 CET 2023")
-            assertThat(res.fitDataPoints[0].altitude).isNull()
-            assertThat(res.fitDataPoints[0].heartRate).isEqualTo(112)
+            assertThat(fitDataPoints[0].lat).isEqualTo(579288959)
+            assertThat(fitDataPoints[0].lon).isEqualTo(104129988)
+            assertThat(fitDataPoints[0].timestamp.toString()).isEqualTo("Sun Dec 17 09:28:40 CET 2023")
+            assertThat(fitDataPoints[0].altitude).isNull()
+            assertThat(fitDataPoints[0].heartRate).isEqualTo(112)
+
+            assertThat(fitSession.sport).isEqualTo(Sport.WALKING)
+            assertThat(fitSession.startTime.toString()).isEqualTo("Sun Dec 17 09:28:39 CET 2023")
+            assertThat(fitSession.totalAscent).isEqualTo(886)
+            assertThat(fitSession.totalDescent).isEqualTo(620)
+            assertThat(fitSession.totalCalories).isEqualTo(1593)
+            assertThat(fitSession.totalDistance).isEqualTo(35148.23f)
+            assertThat(fitSession.totalElapsedTime).isEqualTo(23921.51f)
+            assertThat(fitSession.totalTimerTime).isEqualTo(22558.66f)
         }
 
     }
