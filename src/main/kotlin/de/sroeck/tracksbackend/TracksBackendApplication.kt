46f48a5bb0e7714f47e0026b186c54fb57f6c784
Stefan Roeck <stefan.roeck@gmail.com>
2024-04-30 21:21:00 +0200

    enable gzip compression for XML gpx data
    separate public and internal props

--- src/main/kotlin/de/sroeck/tracksbackend/TracksBackendApplication.kt
+++ src/main/kotlin/de/sroeck/tracksbackend/TracksBackendApplication.kt
@@ -2,10 +2,12 @@ package de.sroeck.tracksbackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.PropertySource
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties", "classpath:secrets.properties")
 class TracksBackendApplication
 
 fun main(args: Array<String>) {
-	runApplication<TracksBackendApplication>(*args)
+    runApplication<TracksBackendApplication>(*args)
 }
